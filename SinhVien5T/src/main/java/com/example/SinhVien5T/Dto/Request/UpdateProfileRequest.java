54bdc53cb56b21c2e4b9818a8c8bd4850e3b6108
Duy Kieu <kieuquangduy6@gmail.com>
2026-03-31 00:19:25 +0700

    Profile Request

--- SinhVien5T/src/main/java/com/example/SinhVien5T/Dto/Request/UpdateProfileRequest.java
+++ SinhVien5T/src/main/java/com/example/SinhVien5T/Dto/Request/UpdateProfileRequest.java
@@ -1,4 +1,32 @@
 package com.example.SinhVien5T.Dto.Request;
 
+import com.example.SinhVien5T.Entity.Enum.Gender;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class UpdateProfileRequest {
+    private String firstName;
+    private String lastName;
+    private LocalDate DoB;
+    private Gender gender;
+    private String ethnicity;
+    private String idenNumber;
+    private String university;
+    private String fieldOfStudy;
+    private String courseYear;
+    private String studentCode;
+    private String classCode;
+    private String faculty;
+    private String currentPosition;
+    private String province;
+    private String commune;
+    private String specificAddress;
+    private String provinceTemp;
+    private String communeTemp;
+    private String specificAddressTemp;
+    private String phoneNumber;
+    private String organPosition;
+    private String ydMember;
 }
